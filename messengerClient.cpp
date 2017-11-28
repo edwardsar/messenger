22a3001791bf4e663c4266f7eade57b34ecd010d
Alex Edwards <edwardsar65@gmail.com>
2017-11-27 22:58:50 -0800

    Add files via upload

---
+++ messengerClient.cpp
@@ -0,0 +1,175 @@
+// Alexander Edwards
+// Student ID: 1188902
+// CISP 360 - Tuesday 4:30-5:50pm
+// This is the client for a simple message sending program
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <thread>
+#pragma comment(lib, "Ws2_32.lib")
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+using std::vector;
+using std::thread;
+
+void setUsername(string &);
+void configServer(struct sockaddr_in &);
+void registerUser(string &, struct sockaddr_in &, SOCKET &, int);
+void sendMessage( struct sockaddr_in &, SOCKET &, int, string& );
+void displayMessages(vector<string>&);
+void messageReceivingWorker(vector<string>&, bool &);
+#define DEFAULT_PORT 1234
+#define CLIENT_PORT 4321
+
+int main()
+{
+    vector<string> messages; //this holds a the messages sent to the user
+    //initialize winsock
+    WSADATA wsaData; //An object that holds information about Windows sockets in use by the host machine
+    if( WSAStartup(MAKEWORD(2,2), &wsaData) != 0 ){ //initializes the wsaData object with Windows Socket implementation details
+        cout << "WSAStartup failed" << endl;        //MAKEWORD(2,2) askes for details for Windows Sockets ver 2.2
+        return 1;
+    }
+    //creating socket
+    SOCKET serverSocket; //declares and initializes a socket variable
+    serverSocket = INVALID_SOCKET;
+    serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); //creating listening socket
+    if(serverSocket == INVALID_SOCKET){ //error checking to make sure socket was created
+        cout << "Error creating socket" << endl;
+        WSACleanup();
+        return 1;
+    }
+    //setting up username and server info and connecting
+    string username;
+    struct sockaddr_in serveraddr; //sockaddr_in struct is used to specify an endpoint address for a socket
+    int serveraddrSize = sizeof(serveraddr);
+    setUsername(username);
+    configServer(serveraddr);
+    registerUser(username, serveraddr, serverSocket, serveraddrSize);
+    bool continueFlag = true;
+    thread receivingMessagesThread(messageReceivingWorker, std::ref(messages), std::ref(continueFlag ) ); //creates a thread that runs the message receiving function
+    
+    int choice;
+    do{
+        cout << "\n\n" << username << ", how would you like to proceed?" << endl
+            << "1. Send a message to a user" << endl
+            << "2. Read your messages" << endl
+            << "3. Quit" << endl
+            << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+        switch(choice){
+            case 1: sendMessage(serveraddr, serverSocket, serveraddrSize, username) ;break;
+            case 2: displayMessages(messages);break;
+            case 3: continueFlag = false ;break;
+            default: ;break;
+        }
+    }while(choice != 3);
+     WSACleanup(); //properly frees network resources used by the server program
+    return 0;
+}
+void setUsername(string &uname)
+{
+    cout << "Enter A username: ";
+    cin >> uname;
+}
+void configServer(struct sockaddr_in &server)
+{
+    string serverIP;
+    cout << "Enter server IP (example: 192.168.44.30): ";
+    cin >> serverIP;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(DEFAULT_PORT);
+    server.sin_addr.s_addr = inet_addr( serverIP.c_str() );
+}
+void registerUser(string &uname, struct sockaddr_in &serveraddr, SOCKET &serverSocket, int serveraddrSize)
+{
+    const int cmdBufferSize = 64;
+    char cmdBuffer[cmdBufferSize];
+    ZeroMemory(cmdBuffer, cmdBufferSize); //clears the buffer of any data
+    strcpy(cmdBuffer, "reg "); //following build the command to send to the server
+    strncat(cmdBuffer, uname.c_str(), cmdBufferSize - sizeof("reg "));
+    sendto(serverSocket, cmdBuffer, cmdBufferSize, 0, (struct sockaddr *) &serveraddr, serveraddrSize); //sends to the server
+}
+void sendMessage( struct sockaddr_in &serveraddr, SOCKET &serverSocket, int serveraddrSize, string &uname)
+{
+    const int bufferSize = 64;
+    const int messageBufferSize = 128;
+    char messageBuffer[messageBufferSize];
+    char mbuf[messageBufferSize];
+    char cmdBuffer[bufferSize];
+    char serverResponseBuffer[bufferSize];
+    const int usernameBufferSize = bufferSize - sizeof("query ");
+    char recipientUsername[usernameBufferSize];
+    ZeroMemory(recipientUsername, usernameBufferSize);
+    cout << "Which user would you like to send too? ";
+    cin.getline(recipientUsername, usernameBufferSize);
+    ZeroMemory(cmdBuffer, bufferSize);
+    strcpy(cmdBuffer, "query ");
+    strncat(cmdBuffer, recipientUsername, usernameBufferSize);
+    sendto(serverSocket, cmdBuffer, bufferSize, 0, (struct sockaddr *) &serveraddr, serveraddrSize);
+    recvfrom(serverSocket, serverResponseBuffer, bufferSize, 0, (struct sockaddr *) &serveraddr, &serveraddrSize);
+    if( strcmp(serverResponseBuffer, "NF") != 0){
+        struct sockaddr_in recipientaddr;
+        int recipientaddrSize = sizeof(recipientaddr);
+        recipientaddr.sin_family = AF_INET;
+        recipientaddr.sin_addr.s_addr = inet_addr( serverResponseBuffer );
+        recipientaddr.sin_port = htons ( CLIENT_PORT );
+        cout << "Begin writing your message: ";
+        cin.getline(mbuf, messageBufferSize);
+        strcpy(messageBuffer, uname.c_str());
+        strncat(messageBuffer, ": ", 2);
+        strncat(messageBuffer, mbuf, 100);
+        sendto(serverSocket, messageBuffer, messageBufferSize, 0, (struct sockaddr *) &recipientaddr, recipientaddrSize);
+    }
+    else{
+        cout << "User doesn't exist." << endl;
+    }
+}
+
+void messageReceivingWorker(vector<string> &m, bool &cont)
+{
+    SOCKET rSocket;
+    rSocket = INVALID_SOCKET;
+    rSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(rSocket == INVALID_SOCKET){
+        cout << "Error creating socket" << endl;
+        WSACleanup();
+    }
+    struct sockaddr_in rServer;
+    rServer.sin_family = AF_INET;
+    rServer.sin_addr.s_addr = INADDR_ANY;
+    rServer.sin_port = htons(CLIENT_PORT);
+    if( (bind(rSocket, (struct sockaddr *) &rServer, sizeof(rServer) )) == SOCKET_ERROR ){
+        cout << "bind failed" << endl;
+        closesocket(rSocket);
+        WSACleanup();
+    }
+    const int messageBufferSize = 128;
+    char messageBuffer[messageBufferSize];
+
+    struct sockaddr_in senderaddr;
+    senderaddr.sin_family = AF_INET;
+    int senderaddrSize = sizeof(senderaddr);
+    while(cont){
+        ZeroMemory(messageBuffer, messageBufferSize);
+        recvfrom(rSocket, messageBuffer, messageBufferSize, 0, (struct sockaddr *) &senderaddr, &senderaddrSize);
+        m.push_back( messageBuffer );
+    }
+}
+
+void displayMessages(vector<string> &m)
+{
+    cout << endl;
+    cout <<"Inbox:\n----------------------------------------------\n";
+    for(int i = 0; i < m.size(); i++){
+        cout << m[i] << endl;
+    }
+    cout << "\n----------------------------------------------\n";
+}
---
+++ messengerServer.cpp
@@ -0,0 +1,102 @@
+// Alexander Edwards
+// Student ID: 1188902
+// CISP 360 - Tuesday 4:30-5:50pm
+// This is a simple server for a message sending program
+// Help and tutorials from:
+// http://beej.us/net2/html/index.html
+// http://www.binarytides.com/code-tcp-so
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms740632(v=vs.85).aspx
+// http://johnnie.jerrata.com/winsocktutorial/
+// https://tangentsoft.net/wskfaq/newbie.html#normalclose
+#include <winsock2.h> //This header provides all the necessary definitions, structures, and functions dealing with network sockets
+#include <ws2tcpip.h> //Provides functions for get IP addresses
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <map> //provides a hashmap for user database
+#pragma comment(lib, "Ws2_32.lib") //links to Ws2_32.lib a necessary library for using Windows network sockets.
+
+using namespace std;
+#define DEFAULT_PORT 1234
+int main()
+{
+    map<string, string> userIPDatabase; //This will hold username, ip pairs in char arrays
+    //initialize winsock
+    cout << "Initializing Winsock" << endl;
+    WSADATA wsaData; //An object that holds information about Windows sockets in use by the host machine
+    if( WSAStartup(MAKEWORD(2,2), &wsaData) != 0 ){ //initializes the wsaData object with Windows Socket implementation details
+        cout << "WSAStartup failed" << endl;        //MAKEWORD(2,2) askes for details for Windows Sockets ver 2.2
+        return 1;
+    }
+    //create a socket
+    cout << "Creating socket" << endl;
+    SOCKET listener = INVALID_SOCKET; //declares and initializes a socket variable
+    listener = socket(AF_INET, SOCK_DGRAM, 0); //creating listening socket
+    if(listener == INVALID_SOCKET){ //error checking to make sure socket was created 
+        cout << "Error creating socket" << endl;
+        WSACleanup();
+        return 1;
+    }
+    //bind the socket
+    cout << "Binding to socket" << endl;
+    struct sockaddr_in serveraddr; //sockaddr_in struct is used to specify an endpoint address for a socket
+    serveraddr.sin_family = AF_INET; //declares IPv4 socket type
+    serveraddr.sin_addr.s_addr = INADDR_ANY; //auto-fill my IP
+    serveraddr.sin_port = htons(DEFAULT_PORT); //sets port and in the right byte order for network
+    if ( bind(listener,(struct sockaddr *)&serveraddr, sizeof(serveraddr) ) == SOCKET_ERROR ) { //performs the socket binding and checks that it was successful
+        cout << "bind failed" << endl;
+        closesocket(listener);
+        WSACleanup();
+        return 1;
+    }
+
+    //listen on the socket for client
+    cout << "Server listening on port: " << ntohs(serveraddr.sin_port) << endl;
+
+    //accept a connection from client
+    struct sockaddr_in clientaddr; //This will hold connecting
+    clientaddr.sin_family = AF_INET; //Indicates client address will be IPv4
+    int clientaddrSize = sizeof(clientaddr); //stores size of clientaddr struct so that casting will work appropriately in call to accept
+    
+    while(true){
+        const int bufferSize = 64;
+        char cmdBuffer[bufferSize];
+        char responseBuffer[bufferSize]; //buffer to hold data for client
+        ZeroMemory(cmdBuffer, bufferSize); // fills arrays with zeros
+        ZeroMemory(responseBuffer, bufferSize);
+        recvfrom(listener, cmdBuffer, bufferSize, 0, (struct sockaddr *) &clientaddr, &clientaddrSize);
+        cout << "Client connected [" << inet_ntoa(clientaddr.sin_addr) << ":" << ntohs(clientaddr.sin_port) <<"]\n";
+        // receive and send data
+        char * token; //used to parse commands from client
+        token = strtok(cmdBuffer, " "); // begins parsing command
+        if( strcmp(token, "reg") == 0){
+            token = strtok(NULL, "\n");
+            cout << "--[Registering User] " << token << " : " << inet_ntoa(clientaddr.sin_addr) << endl; //inet_ntoa produces a string from stored ip addres
+            userIPDatabase[token] = inet_ntoa(clientaddr.sin_addr); // stores username and ip in database
+        }
+        else if( strcmp(token, "query") == 0) {
+            token = strtok(NULL, "\n");
+            cout << "--[Query User] " << token << " -> ";
+            map<string,string>::iterator itr;
+            itr = userIPDatabase.find(token); //iterator itr points to end() if there is no username IP pair found 
+            if(itr != userIPDatabase.end()){
+                cout << userIPDatabase[token] << endl;
+                strncpy(responseBuffer, userIPDatabase[token].c_str(),  bufferSize); // builds response ip for client
+            }
+            else {
+                cout << "Not Found" << endl;
+                strcpy(responseBuffer,"NF"); //if username doesn't have ip associated then NF will be sent to client
+            }
+            sendto(listener, responseBuffer, bufferSize, 0, (struct sockaddr *) &clientaddr, clientaddrSize);
+        }
+        else{
+            cout << "Invalid request" << endl;
+        }
+
+    //disconnect
+    
+    }
+    closesocket(listener); //closes the socket just like closing a file
+    WSACleanup(); //properly frees network resources used by the server program
+    return 0;
+}
